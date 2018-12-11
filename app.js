6d116d0c6ac0b1315ecaea3bf4ff27a5db08c023
b.palowski <brianpalowski00@gmail.com>
2018-12-11 13:37:20 -0800

    Able to append li

--- app.js
+++ app.js
@@ -17,6 +17,25 @@ function addTask(e) {
   if (taskInput.value === "") {
     alert("Add task");
   }
+  /// crete Li
+  const li = document.createElement("li");
+
+  li.className = "collection-item";
+  // Create Text node Apppend to li
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //Create new link element
+  const link = document.createElement("a");
+
+  link.className = "delete-item secondary-content";
+
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  taskInput.value = "";
 
   e.preventDefault();
 }
